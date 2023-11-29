d5213a21dd0e222f739318b78e5f78568810a0da
Dave <l.lopez.david@gmail.com>
2023-11-29 12:53:25 -0600

    se modifica auth con github

--- config/passportGit.config.js
+++ config/passportGit.config.js
@@ -3,6 +3,7 @@ import { Strategy as gitHubStrategy } from 'passport-github2'
 import User from '../src/models/users.model.js'
 import config from './config.env.js'
 import { UserGitDTO } from '../src/dto/userGit.dto.js'
+import url from 'url'
 const { CLIENT_ID, CLIENT_SECRET, CALLBACK_URL } = config
 
 const initializePassportGit = () => {
@@ -12,7 +13,7 @@ const initializePassportGit = () => {
             {
                 clientID: CLIENT_ID,
                 clientSecret: CLIENT_SECRET,
-                callbackURL: `http://${req.get('host')}${CALLBACK_URL}`
+                callbackURL: `http://${url.host}${CALLBACK_URL}`
             }, async (accessToken, refreshToken, profile, done) => {
                 try {
                     let gitUserDTO = new UserGitDTO(profile._json)
